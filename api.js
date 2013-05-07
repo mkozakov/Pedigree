619ff3509efb132b17d155abaef3c4a2b2d89f0b
Michael <michael.kozakov@gmail.com>
2013-05-07 12:45:18 +0300

    HTML stuff

---
+++ api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AbstractHoverbox",
+        "AbstractNode",
+        "AbstractNodeVisuals",
+        "AbstractPerson",
+        "AbstractPersonVisuals",
+        "ActionStack",
+        "ClassName",
+        "Graph",
+        "GroupHoverbox",
+        "Legend",
+        "NodeMenu",
+        "NodetypeSelectionBubble",
+        "Partnership",
+        "PartnershipHoverbox",
+        "PartnershipVisuals",
+        "PedigreeEditor",
+        "Person",
+        "PersonGroup",
+        "PersonGroupVisuals",
+        "PersonHoverbox",
+        "PersonVisuals",
+        "PlaceHolder",
+        "PlaceHolderVisuals",
+        "Pregnancy",
+        "PregnancyVisuals",
+        "SaveLoadEngine",
+        "SaveLoadIndicator",
+        "Workspace"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
